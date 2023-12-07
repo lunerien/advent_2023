7b06e9b13cc49dd877fb65810ac99e2d3738c6b0
lunerien <lukasz.kala6@gmail.com>
2023-12-07 21:34:04 +0100

    day 2 part2 task

--- adv2.py
+++ adv2.py
@@ -1,5 +1,5 @@
 import re
-from collections import Counter
+from collections import Counter, defaultdict
 from enum import Enum
 
 
@@ -19,7 +19,6 @@ def main_adv2() -> int:
         valid_counter = 0
         for line in input_file:
             is_valid = True
-            print(line)
             line = line.replace("Game ", "")
             line_split = line.split(":")
             game_num = int(line_split[0])
@@ -33,11 +32,7 @@ def main_adv2() -> int:
                 for el in match:
                     type_ob = Cubes(el[1])
                     quantity = int(el[0])
-                    if game_num == 80:
-                        print(type_ob, quantity)
                     counter.update({type_ob: quantity})
-                print(counter.most_common())
-
                 if counter[Cubes.green] > GREEN_CUBES:
                     is_valid = False
                     break
@@ -50,10 +45,35 @@ def main_adv2() -> int:
 
             if is_valid:
                 valid_counter += game_num
-            print("is valid: ", is_valid)
-            print("------------------------")
         return valid_counter
 
 
+def main_adv2_part2(file: str) -> int:
+    with open(file, mode="r", encoding="utf-8") as input_file:
+        sums = 0
+        for line in input_file:
+            line = line.replace("Game ", "")
+            line_split = line.split(":")
+
+            throws = line_split[1].split(";")
+            max_val = defaultdict(int)
+            for throw in throws:
+                regex = re.compile(r"(\d+\s)(red|green|blue)")
+                match = regex.findall(throw)
+
+                for el in match:
+                    type_ob = Cubes(el[1])
+                    quantity = int(el[0])
+                    max_val[type_ob] = max(quantity, max_val[type_ob])
+                multi = 1
+            for key, val in max_val.items():
+                multi *= val
+            sums += multi
+        return sums
+
+
 if __name__ == "__main__":
-    print("Adv day 2 valid: ", main_adv2())
+    print("Adv day 2 valid:", main_adv2())
+    print("Adv day 2 part 2:", main_adv2_part2("input_day2"))
+    print("Adv day 2 part 2 test:", main_adv2_part2("input_2_test") == 2286, main_adv2_part2("input_2_test"),
+          "should be 2286")
